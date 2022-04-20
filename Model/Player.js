518542ad59a7f981e64980453193682f2c246c27
Juan Sebastian <jsfc2199@gmail.com>
2022-04-20 14:45:41 -0500

    Who wants to be millionarie Sofka javascript

---
+++ Model/Player.js
@@ -0,0 +1,5 @@
+//Constructor Player class
+function Player(name, score){
+    this.name = name.toUpperCase();
+    this.score = score
+}
---
+++ Model/Question.js
@@ -0,0 +1,8 @@
+function Question(question, answer, options, category) {
+    this.question = question;
+    this.answer = answer;
+    this.options = options;
+    this.category = category;
+}
+
+list: [0,500,1000,2000,3000,4000]
