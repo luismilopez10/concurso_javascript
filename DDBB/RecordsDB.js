a8fcf2a05e181417bac6dd2f97af079814381608
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-04-21 14:24:11 -0500

    Adding styles

--- DDBB/RecordsDB.js
+++ DDBB/RecordsDB.js
@@ -1,10 +1,22 @@
 export let recordsDB = [
     {
-        "name": AAA,
-        "score": 100
+        "name": "AAA",
+        "score": 700
     },
     {
-        "name": BBB,
+        "name": "BBB",
         "score": 200
+    },
+    {
+        "name": "CCC",
+        "score": 500
+    },
+    {
+        "name": "DDD",
+        "score": 1200
+    },
+    {
+        "name": "EEE",
+        "score": 5200
     }
 ]
--- View/ViewLogic.js
+++ View/ViewLogic.js
@@ -40,3 +40,5 @@ function displayQuestion(questionArray){
 function sendAnswer(){
     
 }
+
+
