3fb461d26f494a014f469d00a7feb311dde7c258
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-04-21 12:16:31 -0500

    Styles to home and game

--- View/ViewLogic.js
+++ View/ViewLogic.js
@@ -1,17 +1,13 @@
 import { getQuestionByCategory } from "../BusinessRule/Question.js";
 
-//document.querySelector('input[name="question1"]').value = 'Whatever you want!';
-
 let start = document.querySelector("#send-primary-button");
 
 start.addEventListener("click", ()=>{getQuestionArray(1)});
-
 
 function getQuestionArray(category){
     var questionArray = getQuestionByCategory(category);
     // return questionArray;
     displayQuestion(questionArray);
-
 }
 
 function displayQuestion(questionArray){
@@ -20,8 +16,7 @@ function displayQuestion(questionArray){
 
     let display_question = questionArray[numQuestion].question;
     let textField = document.querySelector("#questionField");
-    textField.value = display_question; 
-    
+    textField.value = display_question;     
     
     
     let display_op1 = document.querySelector('label[for="option1"]');
@@ -35,23 +30,7 @@ function displayQuestion(questionArray){
 
     let display_op4 = document.querySelector('label[for="option4"]');
     display_op4.innerHTML = questionArray[numQuestion].option[3];
-
-
-
-
-
-    // document.querySelector("#op2").innerHTML = questionArray[numQuestion].option[1];
-    // document.querySelector("#op3").innerHTML = questionArray[numQuestion].option[2];
-    // document.querySelector("#op4").innerHTML = questionArray[numQuestion].option[3];
-
 }
-
-
-
-
-// let i = getQuestionByCategory(1);
-// console.log(i);
-
 
 
 
