b1309838176cd9a67eae81eb9657c4194d050b7f
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-04-20 22:46:18 -0500

    QuestionDB.json restructured as object .js. Also the Question in the BusinessRule was modified by reading this new object

--- BusinessRule/Question.js
+++ BusinessRule/Question.js
@@ -1,22 +1,9 @@
 import { Question } from '../Model/Question.js';
-
-let lstQuestion = [];
+import { preguntas } from '../DDBB/QuestionsDB.js';
 
 export function getQuestionByCategory(inCategory) {
-    fetch('../DDBB/QuestionsDB.json')
-    .then (response => response.json())
-    .then(questions=>{
-        for (let question of questions){
-            const mdlQuestion = new Question();
-            mdlQuestion.question=question.question;
-            mdlQuestion.answer=question.answer;
-            mdlQuestion.option=question.option;
-            mdlQuestion.category=question.category;
-            lstQuestion.push(mdlQuestion);
-        }
-        let filterQuestions = lstQuestion.filter(question => question.category === inCategory);
-        return filterQuestions;
-    })
+    var filterQuestions = preguntas.filter(question => question.category === inCategory);
+    return filterQuestions;
 }
 
-//getQuestionByCategory(1);
+console.log(getQuestionByCategory(1));
