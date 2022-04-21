d0248e7f967202954f0b6148f6c3a7d367a560c0
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-04-21 08:48:53 -0500

    Radio buttons fixed

--- View/ViewLogic.js
+++ View/ViewLogic.js
@@ -1,5 +1,6 @@
 import { getQuestionByCategory } from "../BusinessRule/Question.js";
 
+//document.querySelector('input[name="question1"]').value = 'Whatever you want!';
 
 let start = document.querySelector("#send-primary-button");
 
@@ -23,16 +24,16 @@ function displayQuestion(questionArray){
     
     
     
-    let display_op1 = document.querySelector("#option_1");
+    let display_op1 = document.querySelector('label[for="option1"]');
     display_op1.innerHTML = questionArray[numQuestion].option[0];
 
-    let display_op2 = document.querySelector("#option_2");
+    let display_op2 = document.querySelector('label[for="option2"]');
     display_op2.innerHTML = questionArray[numQuestion].option[1];
 
-    let display_op3 = document.querySelector("#option_3");
+    let display_op3 = document.querySelector('label[for="option3"]');
     display_op3.innerHTML = questionArray[numQuestion].option[2];
 
-    let display_op4 = document.querySelector("#option_4");
+    let display_op4 = document.querySelector('label[for="option4"]');
     display_op4.innerHTML = questionArray[numQuestion].option[3];
 
 
